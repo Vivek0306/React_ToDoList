ab2dd8f13dc0af965d0728ea583507c92495fa59
Vivek Nair <vivekmanju53@gmail.com>
2023-05-23 23:47:08 +0530

    dependency fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import './App.css';
 import Topbar from "./components/topbar/Topbar"
 import Home from "./components/home/Home"
 import Todolist from './components/todolist/Todolist';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 
 
 function App() {
